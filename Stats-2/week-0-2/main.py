4cb3137c4e0d56562159067cd6cbe185c23bf1ae
sourav-iitm <souravbhattacharya@yandex.com>
2024-02-03 11:44:16 +0530

    added week-0-2

---
+++ Stats-2/week-0-2/main.py
@@ -0,0 +1,87 @@
+﻿import xml.etree.ElementTree as ET
+import csv
+
+tree=ET.parse("datafile.xml")
+root=tree.getroot()
+r_tot=len(root)
+
+cpi_com = open('cpi_combined.csv', 'w')
+cpi_rur = open('cpi_rural.csv', 'w')
+cpi_urb = open('cpi_urban.csv', 'w')
+cpi_og  = open('cpi_original.csv','w')
+csvbth=csv.writer(cpi_com)
+csvrur=csv.writer(cpi_rur)
+csvurb=csv.writer(cpi_urb)
+csog=csv.writer(cpi_og)
+
+rnum=0
+while rnum<=r_tot:
+    rur_data=[]
+    com_data=[]
+    urb_data=[]
+    og_data=[]
+    for entry in root.findall('ROW'+str(rnum+1)):
+        rsz=len(root[rnum])
+        if rnum==0:
+            i=0
+            while i+1<=len(root[0]):
+                a=root[rnum][i].tag
+                urb_data.append(a)
+                com_data.append(a)
+                rur_data.append(a)
+                og_data.append(a)
+                i+=1
+                            
+            csvbth.writerow(com_data)
+            csvurb.writerow(urb_data)
+            csvrur.writerow(rur_data)
+            csog.writerow(og_data)
+                
+        f=root[rnum][0].text
+        
+        rur_data=[]
+        com_data=[]
+        urb_data=[]
+        og_data=[]
+        
+        if f=='Urban': 
+            i=0
+            while i+1<=len(root[rnum]):
+                a=root[rnum][i].text
+                urb_data.append(a)
+                og_data.append(a)
+                i+=1
+            csvurb.writerow(urb_data)
+        
+        if f=='Rural':
+            i=0
+            while i+1<=len(root[rnum]):
+                a=root[rnum][i].text
+                rur_data.append(a)
+                og_data.append(a)
+                i+=1
+            csvrur.writerow(rur_data)
+        
+        if f=='Rural+Urban':
+            i=0
+            while i+1<=len(root[rnum]):
+                a=root[rnum][i].text
+                com_data.append(a)
+                og_data.append(a)
+                i+=1
+            csvbth.writerow(com_data)
+        
+        
+        csog.writerow(og_data)                 
+        
+        
+            
+                
+            
+            
+        
+    rnum+=1 
+cpi_com.close()
+cpi_rur.close()
+cpi_urb.close()
+cpi_og.close()
---
+++ Stats-2/week-0-2/readme.md
@@ -0,0 +1,10 @@
+This xml file was fetched from the latest dataset available at https://data.gov.in/catalog/all-india-consumer-price-index-ruralurban-0
+
+About the dataset:
+
+This is a dataset for both Rural and Urban Consumer Price Index. Rural, urban, and rural+urban  CPI for each month from June 2013 to May 2023 is given in dataset.xml except for the entry of May 2023 which doesn't have rural+urban CPI data in the original dataset.xml file.
+
+Tasks performed:
+
+i. Parse the xml file in python and export separate csv files for the orignal dataset, dataset for urban only, dataset for rural only and dataset for urban+rural only
+ii. Import the csv files to google docs and draw some meaningful insights
